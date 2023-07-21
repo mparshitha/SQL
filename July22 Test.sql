9bedd42bb3c0e962e5d68b821fb9e79a67aa00e9
mparshitha <mparshitha.xworkz@gmail.com>
2023-07-22 01:53:57 +0530

    july22

---
+++ July22 Test.sql
@@ -0,0 +1,21 @@
+SQL:
+
+1.CREATE DATABASE database_name;
+2.CREATE TABLE table_name(column_names data_types);
+3.ALTER TABLE table_name ADD (column_name data_types);
+4.ALTER TABLE table_name DROP column_name;
+5.ALTER TABLE table_name RENAME column_name data_type to column_name data_type;
+6.ALTER TABLE table_name UPDATE column_name new data_type;
+7.UPDATE table_name SET column1 = value1,...WHERE condition;
+8.DELETE FROM table_name WHERE condition;
+9.SELECT TABLE('character', column_name) AS position FROM table_name;
+10.1.COUNT:SELECT COUNT(*) FROM TABLE;
+   2.SUM:SELECT SUM(column_name) FROM TABLE;
+   3.AVG:SELECT AVG(column_name) FROM table_name; 
+   4.MIN:SELECT MIN(column_name) FROM table_name;
+   5.MAX:SELECT MAX(column_name) FROM table_name; 
+
+
+
+
+
